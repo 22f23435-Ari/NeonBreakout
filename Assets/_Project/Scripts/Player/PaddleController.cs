741a4c7b42b2a80b0a59cdfa3407076da9822a74
Arisha <arishak851@gmail.com>
2026-09-14 21:14:48 +0400

    Implement paddle movement

--- Assets/_Project/Scripts/Player/PaddleController.cs
+++ Assets/_Project/Scripts/Player/PaddleController.cs
@@ -3,7 +3,15 @@ using UnityEngine.InputSystem;
 
 public class PaddleController : MonoBehaviour
 {
+    [SerializeField] private float moveSpeed = 8f;
     [SerializeField] private float moveInput;
+
+    private Rigidbody2D rb;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
 
     private void Update()
     {
@@ -25,5 +33,13 @@ public class PaddleController : MonoBehaviour
         {
             moveInput = 1f;
         }
+    }
+
+    private void FixedUpdate()
+    {
+        Vector2 targetPosition =
+            rb.position + Vector2.right * moveInput * moveSpeed * Time.fixedDeltaTime;
+
+        rb.MovePosition(targetPosition);
     }
 }
